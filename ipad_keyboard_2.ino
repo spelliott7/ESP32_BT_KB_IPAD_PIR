adbbddd37ad530ce3a08cf3d59ae006b00ebccda
everythings12 <39133086+spelliott7@users.noreply.github.com>
2020-04-25 22:04:00 -0500

    Test for opening iPad
    No timer or reset currently. MQTT integration for announcing PIR status.

---
+++ ipad_keyboard_2.ino
@@ -0,0 +1,120 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <BleKeyboard.h>
+
+
+//UPDATE PIN FOR TABLET AT BOTTOM OF CODE
+
+// Wifi: SSID and password
+const PROGMEM char* WIFI_SSID = "WIFI NAME";
+const PROGMEM char* WIFI_PASSWORD = "WIFI PASS";
+
+// MQTT: ID, server IP, port, username and password
+const PROGMEM char* MQTT_CLIENT_ID = "ipad_kb";
+const PROGMEM char* MQTT_SERVER_IP = "MQTT SERVER";
+const PROGMEM uint16_t MQTT_SERVER_PORT = 1883;
+const PROGMEM char* MQTT_USER = "MQTT USER";
+const PROGMEM char* MQTT_PASSWORD = "MQTT PASS";
+
+// MQTT: topic
+const PROGMEM char* MQTT_MOTION_STATUS_TOPIC = "ipad/motion"; //SET topic paramaters here
+
+const PROGMEM char* MOTION_ON = "ON";
+const PROGMEM char* MOTION_OFF = "OFF";
+
+
+const PROGMEM uint8_t PIR_PIN = 27; //ESP32 PIR pin CHANGE AS NEEDED
+uint8_t m_pir_state = LOW; 
+uint8_t m_pir_value = 0;
+
+WiFiClient wifiClient;
+PubSubClient client(wifiClient);
+BleKeyboard bleKeyboard;
+
+void publishPirSensorState() {
+  if (m_pir_state) {
+    client.publish(MQTT_MOTION_STATUS_TOPIC, MOTION_OFF, true);
+  } else {
+    client.publish(MQTT_MOTION_STATUS_TOPIC, MOTION_ON, true);
+  }
+}
+
+void callback(char* p_topic, byte* p_payload, unsigned int p_length) {
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.println("INFO: Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(MQTT_CLIENT_ID, MQTT_USER, MQTT_PASSWORD)) {
+      Serial.println("INFO: connected");
+    } else {
+      Serial.print("ERROR: failed, rc=");
+      Serial.print(client.state());
+      Serial.println("DEBUG: try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  // init the serial
+  Serial.begin(115200);
+
+  // init the WiFi connection
+  Serial.println();
+  Serial.println();
+  Serial.print("INFO: Connecting to ");
+  Serial.println(WIFI_SSID);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("INFO: WiFi connected");
+  Serial.println("INFO: IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // init the MQTT connection
+  client.setServer(MQTT_SERVER_IP, MQTT_SERVER_PORT);
+  client.setCallback(callback);
+
+  Serial.println("Starting BLE work!");
+  bleKeyboard.begin();
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  // read the PIR sensor and send code. Replace 9s
+  m_pir_value = digitalRead(PIR_PIN);
+  if (m_pir_value == HIGH) {
+    if (m_pir_state == LOW) {
+      // a motion is detected
+      Serial.println("INFO: Motion detected");
+      publishPirSensorState();
+      Serial.println("Sending wake command...");
+      bleKeyboard.write(32);
+      delay(100);
+      bleKeyboard.write(32);
+      delay(3500);
+      bleKeyboard.print("9999"); // CHANGE this pin for your iPad
+      bleKeyboard.releaseAll();
+      m_pir_state = HIGH;
+    }
+  } else {
+    if (m_pir_state == HIGH) {
+      publishPirSensorState();
+      m_pir_state = LOW;
+    }
+  }
+}
