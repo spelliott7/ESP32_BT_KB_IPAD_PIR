9d3d17c0818b0d89c8eb451c5677defc91cf9b2c
everythings12 <39133086+spelliott7@users.noreply.github.com>
2020-04-27 19:39:03 -0500

    Update ipad_keyboard_2.ino
    Added mandatory delay to prevent loop of BT command.

--- ipad_keyboard_2.ino
+++ ipad_keyboard_2.ino
@@ -3,33 +3,44 @@
 #include <BleKeyboard.h>
 
 
-//UPDATE PIN FOR TABLET AT BOTTOM OF CODE
 
-// Wifi: SSID and password
-const PROGMEM char* WIFI_SSID = "WIFI NAME";
-const PROGMEM char* WIFI_PASSWORD = "WIFI PASS";
+
+//User selected properties 
+const PROGMEM char* WIFI_SSID = "WiFi SSID";
+const PROGMEM char* WIFI_PASSWORD = "WiFi Pass";
 
 // MQTT: ID, server IP, port, username and password
 const PROGMEM char* MQTT_CLIENT_ID = "ipad_kb";
-const PROGMEM char* MQTT_SERVER_IP = "MQTT SERVER";
+const PROGMEM char* MQTT_SERVER_IP = "MQTT IP/Server info";
 const PROGMEM uint16_t MQTT_SERVER_PORT = 1883;
-const PROGMEM char* MQTT_USER = "MQTT USER";
-const PROGMEM char* MQTT_PASSWORD = "MQTT PASS";
-
-// MQTT: topic
-const PROGMEM char* MQTT_MOTION_STATUS_TOPIC = "ipad/motion"; //SET topic paramaters here
+const PROGMEM char* MQTT_USER = "MQTT User";
+const PROGMEM char* MQTT_PASSWORD = "MQTT Pass";
+const PROGMEM char* PIN_CODE = "0000"; //iPad pin code
+const PROGMEM char* MQTT_MOTION_STATUS_TOPIC = "ipad/motion"; // MQTT topic
+#define timeSeconds 300 //set to number of seconds between activating BT keyboard commands
 
 const PROGMEM char* MOTION_ON = "ON";
 const PROGMEM char* MOTION_OFF = "OFF";
 
 
-const PROGMEM uint8_t PIR_PIN = 27; //ESP32 PIR pin CHANGE AS NEEDED
+const PROGMEM uint8_t PIR_PIN = 27; //ESP32 PIR pin 
 uint8_t m_pir_state = LOW; 
 uint8_t m_pir_value = 0;
+const int led = 26;
+
+unsigned long now = millis();
+unsigned long lastTrigger = 0;
+boolean startTimer = false;
 
 WiFiClient wifiClient;
 PubSubClient client(wifiClient);
 BleKeyboard bleKeyboard;
+
+void IRAM_ATTR detectsMovement() {
+  digitalWrite(led, HIGH);
+  startTimer = true;
+  lastTrigger = millis();
+}
 
 void publishPirSensorState() {
   if (m_pir_state) {
@@ -39,6 +50,7 @@ void publishPirSensorState() {
   }
 }
 
+  
 void callback(char* p_topic, byte* p_payload, unsigned int p_length) {
 }
 
@@ -85,8 +97,14 @@ void setup() {
   client.setServer(MQTT_SERVER_IP, MQTT_SERVER_PORT);
   client.setCallback(callback);
 
-  Serial.println("Starting BLE work!");
+  Serial.println("Ready to send PIN via BT");
   bleKeyboard.begin();
+  pinMode(PIR_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(PIR_PIN), detectsMovement, RISING);
+
+  pinMode(led, OUTPUT);
+  digitalWrite(led, LOW);
+  
 }
 
 void loop() {
@@ -94,8 +112,8 @@ void loop() {
     reconnect();
   }
   client.loop();
-
-  // read the PIR sensor and send code. Replace 9s
+  now = millis();
+  
   m_pir_value = digitalRead(PIR_PIN);
   if (m_pir_value == HIGH) {
     if (m_pir_state == LOW) {
@@ -107,14 +125,15 @@ void loop() {
       delay(100);
       bleKeyboard.write(32);
       delay(3500);
-      bleKeyboard.print("9999"); // CHANGE this pin for your iPad
+      bleKeyboard.print(PIN_CODE); 
       bleKeyboard.releaseAll();
       m_pir_state = HIGH;
+      delay (timeSeconds*1000);
     }
   } else {
     if (m_pir_state == HIGH) {
       publishPirSensorState();
       m_pir_state = LOW;
+      }
     }
   }
-}
